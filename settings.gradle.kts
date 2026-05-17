ea4f19ca8e7de80c1b207b2aa3a24d89767aabb8
Aleksandr Nekrasov <snake_nas@mail.ru>
2026-05-17 17:27:50 +0700

    cleanup spotless

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,3 @@
-rootProject.name = "intellij-quanta-ai-plugin"
-
-include(":shared")
-include(":frontend")
-include(":backend")
-
 pluginManagement {
     repositories {
         mavenLocal()
@@ -11,5 +5,15 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies/")
     }
+}
+
+rootProject.name = "intellij-quanta-ai-plugin"
+
+include(":shared")
+include(":frontend")
+include(":backend")
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
